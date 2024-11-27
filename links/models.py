33775f99a8891a582bbf6d861c1d3ef001cb670f
Maxim Danilov <abl1v1on@yandex.ru>
2024-11-28 04:09:10 +0500

    add date_created and tags for link model

--- links/models.py
+++ links/models.py
@@ -7,9 +7,22 @@ class Link(models.Model):
     short_link = models.CharField('Короткая ссылка', max_length=100, unique=True)
     redirects_count = models.PositiveIntegerField('Кол-во переходов', default=0)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='links')
+    date_created = models.DateField(auto_now_add=True, editable=False)
+    tags = models.ManyToManyField('Tag', related_name='links')
 
     class Meta:
         db_table = 'links'
 
     def __str__(self) -> str:
         return self.source_link
+
+
+class Tag(models.Model):
+    name = models.CharField('Название', max_length=50, unique=True)
+    slug = models.SlugField('URL', max_length=50, db_index=True, unique=True)
+
+    class Meta:
+        db_table = 'tags'
+    
+    def __str__(self) -> str:
+        return self.name
