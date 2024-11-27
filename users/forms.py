cce1eceaf881be9118a183eb4a71eb8afe9545cf
Maxim Danilov <abl1v1on@yandex.ru>
2024-11-28 03:23:25 +0500

    update form for login user view

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control mb-3',
+            'placeholder': 'Введите имя пользователя'
+        }
+    ))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control mb-3',
+            'placeholder': 'Введите пароль'
+        }
+    ))
--- users/views.py
+++ users/views.py
@@ -1,13 +1,26 @@
 from typing import Any
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic import CreateView
+
+from .forms import LoginUserForm
 
 
 class LoginUserView(LoginView):
     template_name = 'users/login.html'
-    form_class = AuthenticationForm
+    form_class = LoginUserForm
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['title'] = 'Авторизация'
+        return context
+
+
+class RegisterUser(CreateView):
+    template_name = 'users/register.html'
+    form_class = UserCreationForm
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация'
         return context
